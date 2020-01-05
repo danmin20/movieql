1bf1bed0ca601764373ce636852c01694738acef
Lee Jeong Min <danmin20@khu.ac.kr>
2020-01-06 01:21:01 +0900

    schema updated

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,14 +1,11 @@
-import { getMovies, getById, addMovie, deleteMovie } from "./db"
+import { getMovies } from "./db";
 
 const resolvers = {
-    Query: {
-        movies: () => getMovies(),
-        movie: (_, { id }) => getById(id)
-    },
-    Mutation: {
-        addMovie: (_, {name, score}) => addMovie(name, score),
-        deleteMovie: (_, {id}) => deleteMovie(id)
-    }
-}
+  Query: {
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id)
+  }
+};
 
 export default resolvers;
